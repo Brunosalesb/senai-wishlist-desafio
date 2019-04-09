7d7165d7bd057a5a18b6950be50d58d9948a5c77
Bruno Sales Bernardes <51418943800@info.local>
2019-04-09 17:17:05 -0300

    fazendo login

--- senai-wishlist-react/src/Pages/Login/App.js
+++ senai-wishlist-react/src/Pages/Login/App.js
@@ -1,14 +1,78 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from '../../Assets/img/wishlist_fundo_escuro.png';
+import '../../Assets/css/app.css';
+import Axios from 'axios'
 
-class App extends Component {
+export default class App extends Component {
+ 
+  
+  constructor(){
+    super();
+    this.state ={
+      email : '',
+      senha :''
+    }
+  }
+  
+                atualizaEstadoEmail(event){
+                  this.setState({email : event.target.value});
+              }
+              atualizaEstadoSenha(event){
+                  this.setState({senha : event.target.value});
+              }
+  
+  efetuaLogin(event){
+    event.preventDefault();
+    Axios.post('http://localhost:5000/api/login',{
+      email : this.state.email,
+      senha : this.state.senha
+    })
+    .then(data => {
+      localStorage.setItem("wishlist-token", data.data.token);
+      this.props.history.push('/cadastrardesejo');
+      console.log(data);
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+  }
+
   render() {
     return (
-      <div>
-         Login
+      <section>
+      <div className="topo">
+          <img src={logo} alt="logo wishlist"></img>
+          <h1>WishList</h1>
       </div>
-    );
-  }
-}
 
-export default App;
+      <form onSubmit={this.efetuaLogin.bind(this)}>
+      <div className="emailInput">
+            <input
+              id="email"
+              type="text"
+              value={this.state.email}
+              onChange={this.atualizaEstadoEmail.bind(this)}
+              placeholder="Email"
+              name="email"
+            />
+      </div>
+
+      <div className="senhaInput">
+            <input id="senha"
+              type="password"
+              value={this.state.senha}
+              onChange={this.atualizaEstadoSenha.bind(this)}
+              placeholder="Senha"
+              name="senha"
+            />
+      </div>
+
+      <div className="botao">
+          <button type="submit">Entrar</button>
+      </div>
+      </form>
+      </section>
+    )
+  } 
+}
--- senai-wishlist-react/src/index.js
+++ senai-wishlist-react/src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Pages/Login/App';
-import Login from "./Pages/Login/App";
 import Cadastro from "./Pages/Cadastro/Cadastro";
 import CadastrarDesejo from "./Pages/CadastrarDesejo/CadastrarDesejo";
 import Listardesejo from "./Pages/ListarDesejo/ListarDesejo";
 import NaoEncontrada from "./Pages/NaoEncontrada/NaoEncontrada";
 import * as serviceWorker from './serviceWorker';
-import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom';
+import {usuarioAutenticado} from './services/auth';
+
+const Permissao = ({component : Component}) => (
+    <Route
+    render = {props => usuarioAutenticado() ?
+        (<Component {...props} />) :
+        (<Redirect to={{pathname :  '/',state : {from : props.location}}}/>)
+    }
+    />
+);
 
 const rotas = (
     <Router>
@@ -16,9 +25,8 @@ const rotas = (
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route path="/cadastro" component={Cadastro} />
-                <Route path="/login" component={Login}/>
-                <Route path="/cadastrardesejo" component={CadastrarDesejo} />
-                <Route path="/listardesejo" component={Listardesejo} />
+                <Permissao path="/cadastrardesejo" component={CadastrarDesejo} />
+                <Permissao path="/listardesejo" component={Listardesejo} />
                 <Route component={NaoEncontrada} />
             </Switch>
         </div>
